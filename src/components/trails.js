44963052a8d06724db5fca6a7dec068e935db685
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-02-03 10:32:22 -0800

    Adds trails

---
+++ src/components/trails.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Trails = (props) => {
+  
+ return(
+ <section>
+  <h3>Results from the Hiking Project API</h3>
+  <ul className="trails-results">
+  { props.trails.map((trail, idx) => 
+      <li> 
+        <p>Hike Name: <a href={trail.trail_url}>{trail.name}</a>, Location: {trail.location}, Distance: {trail.length} miles</p>
+        <p>On {trail.condition_date} at {trail.condition_time}, trail conditions were reported as: {trail.conditions}</p>
+        <p>This trail has a rating of {trail.stars} stars (out of {trail.star_votes} votes)</p>
+        <p>{trail.summary}</p>
+
+      </li>
+  ) }  
+  </ul>
+</section>
+ );
+}
+
+export default Trails;
