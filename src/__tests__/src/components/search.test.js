bc3425f23ae5b60a5acb4b7aa36632907ee36929
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-02-03 16:39:42 -0800

    adds testing

---
+++ src/__tests__/src/components/search.test.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import renderer from "react-test-renderer";
+import Search from "../../../components/search.js";
+import When from "../../../components/when.js";
+import Weather from "../../../components/data/weather.js";
+
+
+describe('Search', () => {
+  it('lives', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('contains a map image', () => {
+    let component = shallow(<Search/>);
+    expect(component.find('img').exists()).toBeTruthy();
+  });
+
+  it('generates an h2 tag', () => {
+    let component = shallow(<Search/>);
+    expect(component.find('h2').exists()).toBeTruthy();
+  })
+
+  it('changes on submit', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('img')).toBeTruthy();
+    expect(component.find('#query-container')).toBeTruthy();
+  });
+
+  it('renders the map', () => {
+      let component = mount(<Search/>);
+      let submission = component.find('form');
+      submission.simulate('submit');
+      expect(component.find('#map')).toBeTruthy();
+  });
+
+  it('renders the columns', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('#column-container')).toBeTruthy();
+  });
+
+  it('renders the columns', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('#column-container')).toBeTruthy();
+  });
+
+  it('renders the weather', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('.weather-results')).toBeTruthy();
+  });
+
+  it('renders yelp', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('.yelp-results')).toBeTruthy();
+  });
+
+  it('renders movies', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('.movies-results')).toBeTruthy();
+  });
+
+  it('renders trails', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('.trails-results')).toBeTruthy();
+  });
+
+  it('renders meetups', () => {
+    let component = mount(<Search/>);
+    let submission = component.find('form');
+    submission.simulate('submit');
+    expect(component.find('.meetups-results')).toBeTruthy();
+  });
+
+
+});
